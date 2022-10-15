521a0656e80e70138c3c471e3edbf14dd8b106ee
QuestionableM <77170113+QuestionableM@users.noreply.github.com>
2022-10-15 15:55:04 -0700

    Added support for ground materials

--- Blueprint Converter Plugin/AssignMaterialsFunc.py
+++ Blueprint Converter Plugin/AssignMaterialsFunc.py
@@ -2,138 +2,165 @@ import bpy;
 from .BasicFunc import *;
 
 def HasAnyOf(node_tree, obj_list):
-    obj_count = len(obj_list);
-    for i in range(obj_count):
-        if check_if_has_node(node_tree, obj_list[i]):
-            return True;
+	obj_count = len(obj_list);
+	for i in range(obj_count):
+		if check_if_has_node(node_tree, obj_list[i]):
+			return True;
 
-    return False;
+	return False;
 
 def IsNodeValid(cur_mat):
-    if not is_material_valid(cur_mat.name_full):
-        return False;
+	if not is_material_valid(cur_mat.name_full):
+		return False;
 
-    if cur_mat.node_tree == None:
-        return False;
+	if cur_mat.node_tree == None:
+		return False;
 
-    if HasAnyOf(cur_mat.node_tree.nodes, sm_known_materials):
-        return False;
+	if HasAnyOf(cur_mat.node_tree.nodes, sm_known_materials):
+		return False;
 
-    return True;
+	return True;
 
 def FindNeededNodes(mat_nodes):
-    dif_node = None;
-    asg_node = None;
-    nor_node = None;
+	dif_node = None;
+	asg_node = None;
+	nor_node = None;
 
-    for cur_node in mat_nodes:
-        if cur_node.bl_idname != "ShaderNodeTexImage":
-            continue;
+	for cur_node in mat_nodes:
+		if cur_node.bl_idname != "ShaderNodeTexImage":
+			continue;
 
-        node_links = cur_node.outputs[0];
-        link_amount = len(node_links.links);
-        for b in range(link_amount):
-            cur_link = node_links.links[b];
+		node_links = cur_node.outputs[0];
+		link_amount = len(node_links.links);
+		for b in range(link_amount):
+			cur_link = node_links.links[b];
 
-            cur_link_socket = cur_link.to_socket.name;
-            cur_link_node = cur_link.to_node.name;
+			cur_link_socket = cur_link.to_socket.name;
+			cur_link_node = cur_link.to_node.name;
 
-            if cur_link_socket == 'Color' and cur_link_node == 'Normal Map':
-                nor_node = cur_node;
-                break;
-            elif cur_link_socket == 'Base Color' and cur_link_node == 'Principled BSDF':
-                dif_node = cur_node;
-                break;
-            elif cur_link_socket == 'Specular' and cur_link_node == 'Principled BSDF':
-                asg_node = cur_node;
-                break;
+			if cur_link_socket == 'Color' and cur_link_node == 'Normal Map':
+				nor_node = cur_node;
+				break;
+			elif cur_link_socket == 'Base Color' and cur_link_node == 'Principled BSDF':
+				dif_node = cur_node;
+				break;
+			elif cur_link_socket == 'Specular' and cur_link_node == 'Principled BSDF':
+				asg_node = cur_node;
+				break;
 
-    return dif_node, asg_node, nor_node;
+	return dif_node, asg_node, nor_node;
 
 def GetMaterialOutputNode(mat_nodes):
-    mat_output = None;
+	mat_output = None;
 
-    for cur_node in mat_nodes:
-        cnode_name = cur_node.bl_idname;
+	for cur_node in mat_nodes:
+		cnode_name = cur_node.bl_idname;
 
-        if cnode_name == 'ShaderNodeOutputMaterial':
-            if mat_output == None:
-                mat_output = cur_node;
-            continue;
+		if cnode_name == 'ShaderNodeOutputMaterial':
+			if mat_output == None:
+				mat_output = cur_node;
+			continue;
 
-        if cnode_name == 'ShaderNodeTexImage':
-            continue;
-        
-        mat_nodes.remove(cur_node);
+		if cnode_name == 'ShaderNodeTexImage':
+			continue;
+		
+		mat_nodes.remove(cur_node);
 
-    return mat_output;
+	return mat_output;
 
 def CreateSMTexNode(mat_nodes, color, settings):
-    node_name = settings["name"];
+	node_name = settings["name"];
 
-    sm_tex = mat_nodes.new('ShaderNodeGroup');
-    sm_tex.node_tree = bpy.data.node_groups[node_name];
-    sm_tex.location = (100, 300);
-    sm_tex.name = node_name;
+	sm_tex = mat_nodes.new('ShaderNodeGroup');
+	sm_tex.node_tree = bpy.data.node_groups[node_name];
+	sm_tex.location = (100, 300);
+	sm_tex.name = node_name;
 
-    sm_tex.inputs[settings["color_input_idx"]].default_value = hex_to_rgb(int(color, 16));
+	sm_tex.inputs[settings["color_input_idx"]].default_value = hex_to_rgb(int(color, 16));
 
-    return sm_tex;
+	return sm_tex;
 
-def InitDifNode(dif, links, tex_shader):
-    if dif != None:
-        dif.location = (-300, 300);
-        dif.hide = True;
-        links.new(dif.outputs[0], tex_shader.inputs[0]);
-        links.new(dif.outputs[1], tex_shader.inputs[1]);
+def InitDifNode(dif, links, tex_shader, color_input_idx, alpha_input_idx):
+	if dif != None:
+		dif.location = (-300, 300);
+		dif.hide = True;
+		links.new(dif.outputs[0], tex_shader.inputs[color_input_idx]);
+		links.new(dif.outputs[1], tex_shader.inputs[alpha_input_idx]);
 
-def InitAsgNode(asg, links, tex_shader):
-    if asg != None:
-        asg.location = (-300, 250);
-        asg.hide = True;
-        asg.image.colorspace_settings.name='Raw';
-        links.new(asg.outputs[0], tex_shader.inputs[3]);
-        links.new(asg.outputs[1], tex_shader.inputs[4]);
+def InitAsgNode(asg, links, tex_shader, color_input_idx, alpha_input_idx):
+	if asg != None:
+		asg.location = (-300, 250);
+		asg.hide = True;
+		asg.image.colorspace_settings.name='Raw';
+		links.new(asg.outputs[0], tex_shader.inputs[color_input_idx]);
+		links.new(asg.outputs[1], tex_shader.inputs[alpha_input_idx]);
 
-def InitNorNode(nor, links, tex_shader):
-    if nor != None:
-        nor.location = (-300, 200);
-        nor.hide = True;
-        nor.image.colorspace_settings.name='Raw';
-        links.new(nor.outputs[0], tex_shader.inputs[7]);
+def InitNorNode(nor, links, tex_shader, color_input_idx):
+	if nor != None:
+		nor.location = (-300, 200);
+		nor.hide = True;
+		nor.image.colorspace_settings.name='Raw';
+		links.new(nor.outputs[0], tex_shader.inputs[color_input_idx]);
 
 def ConnectWithOutputNode(out_node, links, tex_shader):
-    if out_node != None:
-        links.new(tex_shader.outputs[0], out_node.inputs[0]);
+	if out_node != None:
+		links.new(tex_shader.outputs[0], out_node.inputs[0]);
 
 def Assign_Materials_Func(mat_array):
-    for cur_mat in mat_array:
-        if not IsNodeValid(cur_mat):
-            continue;
+	for cur_mat in mat_array:
+		if not IsNodeValid(cur_mat):
+			continue;
 
-        mat_sep_name = cur_mat.name_full.split(sep=" ");
-        mat_sep_name_len = len(mat_sep_name);
+		material_index = "1";
+		material_color = "ffffff";
 
-        material_index = "1";
+		dif_color_input_idx = 0
+		dif_alpha_input_idx = 1
 
-        if mat_sep_name_len == 4:
-            material_index = mat_sep_name[3][1:];
-            mat_dot_idx = material_index.find(".");
+		asg_color_input_idx = 3
+		asg_alpha_input_idx = 4
 
-            if mat_dot_idx > -1:
-                material_index = material_index[:mat_dot_idx];
+		nor_color_input_idx = 7
 
-        cur_node_data = sm_material_table[material_index];
+		whitelisted_data = get_whitelisted_material_data(cur_mat.name_full);
+		if whitelisted_data == None:
+			mat_sep_name = cur_mat.name_full.split(sep=" ");
+			mat_sep_name_len = len(mat_sep_name);
 
-        mat_nodes = cur_mat.node_tree.nodes;
-        dif_node, asg_node, nor_node = FindNeededNodes(mat_nodes);
-        material_output_node = GetMaterialOutputNode(mat_nodes);
+			material_color = mat_sep_name[1];
 
-        SM_Tex_Shader = CreateSMTexNode(mat_nodes, mat_sep_name[1], cur_node_data);
+			if mat_sep_name_len == 4:
+				material_index = mat_sep_name[3][1:];
+				mat_dot_idx = material_index.find(".");
 
-        node_links = cur_mat.node_tree.links;
-        InitDifNode(dif_node, node_links, SM_Tex_Shader);
-        InitAsgNode(asg_node, node_links, SM_Tex_Shader);
-        InitNorNode(nor_node, node_links, SM_Tex_Shader);
+				if mat_dot_idx > -1:
+					material_index = material_index[:mat_dot_idx];
+		else:
+			material_index = whitelisted_data["index"]
+			material_color = whitelisted_data["color"]
 
-        ConnectWithOutputNode(material_output_node, node_links, SM_Tex_Shader);
+			if "texture_inputs" in whitelisted_data:
+				v_tex_input_data = whitelisted_data["texture_inputs"];
+
+				dif_color_input_idx = v_tex_input_data["dif_color"];
+				dif_alpha_input_idx = v_tex_input_data["dif_alpha"];
+
+				asg_color_input_idx = v_tex_input_data["asg_color"];
+				asg_alpha_input_idx = v_tex_input_data["asg_alpha"];
+
+				nor_color_input_idx = v_tex_input_data["nor_color"];
+
+		cur_node_data = sm_material_table[material_index];
+
+		mat_nodes = cur_mat.node_tree.nodes;
+		dif_node, asg_node, nor_node = FindNeededNodes(mat_nodes);
+		material_output_node = GetMaterialOutputNode(mat_nodes);
+
+		SM_Tex_Shader = CreateSMTexNode(mat_nodes, material_color, cur_node_data);
+
+		node_links = cur_mat.node_tree.links;
+		InitDifNode(dif_node, node_links, SM_Tex_Shader, dif_color_input_idx, dif_alpha_input_idx);
+		InitAsgNode(asg_node, node_links, SM_Tex_Shader, asg_color_input_idx, asg_alpha_input_idx);
+		InitNorNode(nor_node, node_links, SM_Tex_Shader, nor_color_input_idx);
+
+		ConnectWithOutputNode(material_output_node, node_links, SM_Tex_Shader);
--- Blueprint Converter Plugin/BasicFunc.py
+++ Blueprint Converter Plugin/BasicFunc.py
@@ -2,147 +2,178 @@ from asyncio.windows_events import NULL
 import bpy;
 
 sm_known_materials = (
-    "SM_DifAsgNor",
-    "SM_Glass",
-    "SM_Grass",
-    "SM_Leaves",
-    "SM_DifAsgNorAlpha",
-    "SM_Water",
-    "SM_LightCone",
-    "SM_Liquid"
+	"SM_DifAsgNor",
+	"SM_Glass",
+	"SM_Grass",
+	"SM_Leaves",
+	"SM_DifAsgNorAlpha",
+	"SM_Water",
+	"SM_LightCone",
+	"SM_Liquid",
+	"SM_GroundMaterial"
 )
 
 sm_material_table = {
-    "1": {
-        "name": "SM_DifAsgNor",
-        "color_input_idx": 2
-    },
-    "2": {
-        "name": "SM_Glass",
-        "color_input_idx": 2
-    },
-    "3": {
-        "name": "SM_Grass",
-        "color_input_idx": 2
-    },
-    "4": {
-        "name": "SM_Leaves",
-        "color_input_idx": 2
-    },
-    "5": {
-        "name": "SM_DifAsgNorAlpha",
-        "color_input_idx": 2
-    },
-    "6": {
-        "name": "SM_Water",
-        "color_input_idx": 2
-    },
-    "7": {
-        "name": "SM_LightCone",
-        "color_input_idx": 2
-    },
-    "8": {
-        "name": "SM_Liquid",
-        "color_input_idx": 2
-    }
+	"1": {
+		"name": "SM_DifAsgNor",
+		"color_input_idx": 2
+	},
+	"2": {
+		"name": "SM_Glass",
+		"color_input_idx": 2
+	},
+	"3": {
+		"name": "SM_Grass",
+		"color_input_idx": 2
+	},
+	"4": {
+		"name": "SM_Leaves",
+		"color_input_idx": 2
+	},
+	"5": {
+		"name": "SM_DifAsgNorAlpha",
+		"color_input_idx": 2
+	},
+	"6": {
+		"name": "SM_Water",
+		"color_input_idx": 2
+	},
+	"7": {
+		"name": "SM_LightCone",
+		"color_input_idx": 2
+	},
+	"8": {
+		"name": "SM_Liquid",
+		"color_input_idx": 2
+	},
+	"9": {
+		"name": "SM_GroundMaterial",
+		"color_input_idx": 2
+	}
+}
+
+sm_material_whitelist = {
+	"TileGroundTerrain": {
+		"index": "9",
+		"color": "ffffff",
+		"texture_inputs": {
+			"dif_color": 0,
+			"dif_alpha": 1,
+
+			"asg_color": 3,
+			"asg_alpha": 4,
+
+			"nor_color": 6
+		}
+	}
 }
 
 def node_group_exists(name):
-    node_groups = bpy.data.node_groups;
-    node_amount = len(node_groups);
+	node_groups = bpy.data.node_groups;
+	node_amount = len(node_groups);
 
-    for a in range(node_amount):
-        cur_group = node_groups[a];
+	for a in range(node_amount):
+		cur_group = node_groups[a];
 
-        if cur_group.name_full == name:
-            return True;
+		if cur_group.name_full == name:
+			return True;
 
-    return False;
+	return False;
 
 from uuid import UUID;
 
 def is_color_valid(hex_string):
-    try:
-        int(hex_string, 16);
-    except:
-        return False;
+	try:
+		int(hex_string, 16);
+	except:
+		return False;
 
-    return True;
+	return True;
 
 def is_uuid_valid(uuid_string):
-    try:
-        uuid_obj = UUID(uuid_string);
-    except:
-        return False;
+	try:
+		uuid_obj = UUID(uuid_string);
+	except:
+		return False;
 
-    return str(uuid_obj) == uuid_string;
+	return str(uuid_obj) == uuid_string;
 
 def is_mat_index_valid(material_index):
-    mat_idx_str = material_index
+	mat_idx_str = material_index
 
-    dot_idx = mat_idx_str.find(".")
-    if dot_idx > -1:
-        mat_idx_str = mat_idx_str[:dot_idx];
+	dot_idx = mat_idx_str.find(".")
+	if dot_idx > -1:
+		mat_idx_str = mat_idx_str[:dot_idx];
 
-    if mat_idx_str[0] == "m":
-        try:
-            mat_idx_number = mat_idx_str[1:];
-            int_test = int(mat_idx_number);
+	if mat_idx_str[0] == "m":
+		try:
+			mat_idx_number = mat_idx_str[1:];
 
-            if sm_material_table[mat_idx_number] == None:
-                return False;
+			if sm_material_table[mat_idx_number] == None:
+				return False;
 
-            return True;
-        except:
-            return False;
+			return True;
+		except:
+			return False;
 
-    return False;
+	return False;
+
+def get_whitelisted_material_data(mat_name):
+	try:
+		return sm_material_whitelist[mat_name];
+	except:
+		return None;
+
+	return None;
 
 def is_material_valid(mat_name):
-    sep_name = mat_name.split(sep=" ");
-    sep_name_len = len(sep_name);
+	if get_whitelisted_material_data(mat_name) != None:
+		return True;
 
-    if sep_name_len < 3:
-        return False;
+	sep_name = mat_name.split(sep=" ");
+	sep_name_len = len(sep_name);
 
-    uuid_len = len(sep_name[0]);
-    color_len = len(sep_name[1]);
+	if sep_name_len < 3:
+		return False;
 
-    if uuid_len != 36 or color_len != 6:
-        return False;
+	uuid_len = len(sep_name[0]);
+	color_len = len(sep_name[1]);
 
-    if not is_uuid_valid(sep_name[0]) or not is_color_valid(sep_name[1]):
-        return False;
+	if uuid_len != 36 or color_len != 6:
+		return False;
 
-    if sep_name_len == 4 and not is_mat_index_valid(sep_name[3]):
-        return False;
+	if not is_uuid_valid(sep_name[0]) or not is_color_valid(sep_name[1]):
+		return False;
 
-    return True;
+	if sep_name_len == 4 and not is_mat_index_valid(sep_name[3]):
+		return False;
+
+	return True;
 
 
 def check_if_has_node(nodes, name):
-    node_amount = len(nodes);
+	node_amount = len(nodes);
 
-    for a in range(node_amount):
-        cur_node = nodes[a];
+	for a in range(node_amount):
+		cur_node = nodes[a];
 
-        if cur_node.name == name:
-            return True;
+		if cur_node.name == name:
+			return True;
 
-    return False;
+	return False;
 
 def remove_node_links(node_tree, node):
-    for out in node.outputs:
-        for lnk in out.links:
-            node_tree.links.remove(lnk);
+	for out in node.outputs:
+		for lnk in out.links:
+			node_tree.links.remove(lnk);
 
 def srgb_to_linearrgb(c):
-    if   c < 0:       return 0
-    elif c < 0.04045: return c/12.92
-    else:             return ((c+0.055)/1.055)**2.4
+	if   c < 0:       return 0
+	elif c < 0.04045: return c/12.92
+	else:             return ((c+0.055)/1.055)**2.4
 
 def hex_to_rgb(h, alpha=1):
-    r = (h & 0xff0000) >> 16
-    g = (h & 0x00ff00) >> 8
-    b = (h & 0x0000ff)
-    return tuple([srgb_to_linearrgb(c/0xff) for c in (r,g,b)] + [alpha])
+	r = (h & 0xff0000) >> 16
+	g = (h & 0x00ff00) >> 8
+	b = (h & 0x0000ff)
+	return tuple([srgb_to_linearrgb(c/0xff) for c in (r,g,b)] + [alpha])
