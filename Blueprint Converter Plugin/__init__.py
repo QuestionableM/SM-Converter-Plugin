7d42e0441d5169428d0557ee3aef0e3b03e796dc
QuestionableM <77170113+QuestionableM@users.noreply.github.com>
2022-10-16 11:48:30 -0700

    version 0.0.7

--- Blueprint Converter Plugin/__init__.py
+++ Blueprint Converter Plugin/__init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author" : "Questionable Mark",
     "description" : "",
     "blender" : (2, 92, 0),
-    "version" : (0, 0, 6),
+    "version" : (0, 0, 7),
     "location" : "",
     "warning" : "",
     "category" : "Generic"
